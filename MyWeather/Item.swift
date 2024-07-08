83b0c76f43d68e30b0516e84d8bf576e05487854
Willian Honda <willian_honda@apple.com>
2024-07-08 17:18:01 -0300

    Initial Commit

---
+++ MyWeather/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MyWeather
+//
+//  Created by Willian Honda on 08/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
